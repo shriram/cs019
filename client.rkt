c4f86c6d78e8ac1428d134f2cefff62cf2225e29
Shriram Krishnamurthi <sk@cs.brown.edu>
2010-09-02 21:49:58 -0400

    Changing to Teachpack.

--- client.rkt
+++ client.rkt
@@ -1,56 +1,45 @@
-#lang s-exp "cs019.rkt"
-
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname client) (read-case-sensitive #t) (teachpacks ((lib "cs0190tpk.rkt" "installed-teachpacks"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "cs0190tpk.rkt" "installed-teachpacks")))))
 (require (only-in racket/base list->vector))
 (require racket/tcp)
+(provide (all-defined-out))
 
+;; Run (do-worlds) to test big-bang and animate programs
+
+"Testing printer"
 true
-
 '(1 2 3)
-
 (list 1 2 3)
-
-(build-list 10 add1)
-
 22/7
-
 (vector 1 2 3)
-
 (define-struct f (x))
 (make-f 10)
 (make-f (make-f 10))
-
-(define h (make-hash empty))
-
-(define-struct transition (from via to))
-(define sm
-  (list
-   (make-transition "a" "b" "c")
-   (make-transition "d" "e" "f")
-   (make-transition "g" "h" "i")))
-sm
-
-22/7
-(build-list 3 add1)
 (define-struct g (a b))
 (make-g 1 2)
-; This is a real problem:
-;   Actual value (1 2) differs from (3 4), the expected value.
-(check-expect (list 1 2) (list 3 4))
 
-(require 2htdp/image)
+(check-expect (build-list 5 add1) (list 1 2 3 4 5))
+
 (define i (open-image-url "http://racket-lang.org/logo.png"))
-(image-height i)
-(image-width i)
+(check-expect (image-height i) 85)
+(check-expect (image-width i) 88)
 
 (define ht (hash))
 (define ht2 (hash-set ht "x" 10))
-(hash-ref ht2 "x")
+(check-expect(hash-ref ht2 "x") 10)
 (check-error (hash-ref ht "x") "hash-ref: no value found for key: \"x\"")
 
-;(provide (all-defined-out))
+(define cont (open-image-url "http://www.continental.com/web/format/img/header/continentalLogo.gif"))
 
-(define-struct tv (tag value))
-;(provide (struct-out tv))
+(define (draw-circ t)
+  (place-image (circle (modulo t 50) "solid" "red")
+               50
+               50
+               (empty-scene 100 100)))
 
-(big-bang 0 [on-tick add1] 
-            [on-draw (lambda (w) (overlay (circle (modulo w 50) "outline" "red") (empty-scene 100 100)))])
+(define (do-worlds)
+  (begin
+    (big-bang 0 [on-tick add1] 
+              [on-draw (lambda (w) (overlay (circle (modulo w 50) "outline" "red") (empty-scene 100 100)))])
+    (animate draw-circ)))
---
+++ cs019-tpk.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+(require 2htdp/image
+         2htdp/universe)
+
+(provide make-immutable-hash
+         hash
+         hash-set
+         hash-update
+         hash-iterate-first
+         hash-iterate-next
+         hash-iterate-key
+         hash-iterate-value
+         require only-in except-in prefix-in rename-in combine-in
+         provide all-defined-out all-from-out rename-out except-out prefix-out struct-out combine-out protect-out
+         [all-from-out 2htdp/universe]
+         [all-from-out 2htdp/image])
+
+(require net/url
+         racket/list
+         racket/file
+         racket/path
+         racket/port
+         racket/class
+         racket/gui/base
+         racket/contract)
+
+(define (open-image-url a-url-string)
+  (let* ([url (string->url a-url-string)]
+         [ip (get-pure-port url)]
+         [filename (path/param-path (last (url-path url)))]
+         [temp-file-path (normalize-path (make-temporary-file (format "~~a-~a" filename)))])
+    (dynamic-wind
+     (lambda ()
+       (copy-port ip (open-output-file temp-file-path #:exists 'truncate)))
+     (lambda ()
+       (make-object image-snip% (make-object bitmap% temp-file-path)))
+     (lambda ()
+       (delete-file temp-file-path)))))
+          
+(provide/contract [open-image-url (string? . -> . (is-a?/c image-snip%))])
