efe2e12bb54da04d6623de0aa2db0017fd65ed81
Shriram Krishnamurthi <sk@cs.brown.edu>
2011-09-02 13:23:08 -0400

    moved base language to asl because the contract in open-image-url otherwise breaks on expansion into bsl

--- cs019.rkt
+++ cs019.rkt
@@ -1,12 +1,12 @@
 #lang racket
 
 (require racket/provide) ;; to get provide's filtered-out
-(require (prefix-in bsl: (except-in lang/htdp-beginner image? require #%module-begin)))
-(require (only-in lang/htdp-beginner (#%module-begin bsl:module-begin)))
+(require (prefix-in bsl: (except-in lang/htdp-advanced image? require #%module-begin)))
+(require (only-in lang/htdp-advanced (#%module-begin bsl:module-begin)))
 (provide (filtered-out (lambda (name)
                          (and (regexp-match #rx"^bsl:" name)
                               (regexp-replace #rx"^bsl:" name "")))
-                       (all-from-out lang/htdp-beginner)))
+                       (all-from-out lang/htdp-advanced)))
 (provide (rename-out (top-level #%module-begin)))
 
 (require "open-image-url.rkt")
@@ -16,7 +16,8 @@
 (provide [all-from-out 2htdp/universe] [all-from-out 2htdp/image])
 
 ; From Racket:
-(provide make-immutable-hash hash hash-set hash-update
+(provide ;make-immutable-hash
+         hash hash-set hash-update
          hash-iterate-first hash-iterate-next hash-iterate-key hash-iterate-value)
 (provide require only-in except-in prefix-in rename-in combine-in planet)
 (provide provide all-defined-out all-from-out rename-out except-out 
